171518df09ff0383440d51a487ec19d886db20b0
Shashwat_Dwivedi <shashwatsd44@gmail.com>
2024-02-18 18:28:52 +0530

    updated user.js , socket logic

--- chat-app-react-nodejs-master/server/controllers/user.js
+++ chat-app-react-nodejs-master/server/controllers/user.js
@@ -1,209 +1,234 @@
-const User = require('../model/User')
-const Room = require('../model/Room')
-const Message = require('../model/Message')
-const asyncHandler = require('express-async-handler')
+// Importing required models and libraries
+const User = require('../model/User');
+const Room = require('../model/Room');
+const Message = require('../model/Message');
+const asyncHandler = require('express-async-handler');
 
+// Function to get unread message count
 const getUnreadCount = asyncHandler(async (type, from, to) => {
-  const filter = type === 'room' ? [to] : [from, to]
+  // Construct filter based on message type
+  const filter = type === 'room' ? [to] : [from, to];
+  // Query message readers from database
   const messageReaders = await Message
-    .find({ sender: { $ne: from } }) // sender 不是自己的訊息
+    .find({ sender: { $ne: from } }) // Excluding sender's own messages
     .all('users', filter)
     .select(['readers'])
     .sort({ createdAt: -1 })
-    .lean()
+    .lean();
   
-  // readers 裡面沒有自己的 id
-  return messageReaders.filter(({ readers }) => readers.indexOf(from) === -1 ).length || 0
-})
+  // Calculate unread message count
+  return messageReaders.filter(({ readers }) => readers.indexOf(from) === -1).length || 0;
+});
 
+// Function to get latest message info
 const getMessageInfo = asyncHandler(async(type, from, to) => {
-  const filter = type === 'room' ? [to] : [from, to]
+  // Construct filter based on message type
+  const filter = type === 'room' ? [to] : [from, to];
+  // Query latest message from database
   const message = await Message
     .findOne()
     .all('users', filter)
     .select(['message', 'sender', 'updatedAt', 'readers'])
     .sort({ createdAt: -1 })
-    .lean()
+    .lean();
 
-  const unreadCount = await getUnreadCount(type, from, to)
+  // Get unread message count
+  const unreadCount = await getUnreadCount(type, from, to);
 
   return {
     latestMessage: message?.message || null,
     latestMessageSender:  message?.sender || null,
     latestMessageUpdatedAt:  message?.updatedAt || null,
     unreadCount
-  }
-})
+  };
+});
 
-// READ
+// Route handler to get user contacts
 const getUserContacts = asyncHandler(async(req, res) => {
   try {
-    const { userId } = req.params
+    const { userId } = req.params;
 
-    if(!userId) return res.status(400).json({ message: 'Missing required information.' })
+    // Validation check for required information
+    if (!userId) return res.status(400).json({ message: 'Missing required information.' });
 
+    // Query users and rooms from database
     const users = await User
-      .find({ _id: { $ne: userId } })
+      .find({ _id: { $ne: userId } }) // Excluding current user
       .select(['name', 'avatarImage', 'chatType'])
       .sort({ updatedAt: -1 })
-      .lean()
+      .lean();
 
     const rooms = await Room
       .find()
       .all('users', [userId])
       .select(['name', 'users', 'avatarImage', 'chatType'])
       .sort({ updatedAt: -1 })
-      .lean()
+      .lean();
       
-    const contacts = users.concat(rooms)
+    // Combine users and rooms
+    const contacts = users.concat(rooms);
+    // Get message info for each contact
     const contactWithMessages = await Promise.all(
       contacts.map(async(contact) => {
-        const { _id, chatType: type } = contact
-        const messageInfo = await getMessageInfo(type, userId, _id.toHexString())
+        const { _id, chatType: type } = contact;
+        const messageInfo = await getMessageInfo(type, userId, _id.toHexString());
 
         return {
           ...contact,
           ...messageInfo
-        }
+        };
       })
-    )
+    );
 
-    return res.status(200).json({ data: contactWithMessages })
+    return res.status(200).json({ data: contactWithMessages });
   } catch(err) {
-    return res.status(404).json({ message: err.message })
+    return res.status(404).json({ message: err.message });
   }
-})
+});
 
+// Route handler to get user messages
 const getUserMessages = asyncHandler(async(req, res) => {
   try {
-    const { userId } = req.params
-    const { type, chatId } = req.query
+    const { userId } = req.params;
+    const { type, chatId } = req.query;
 
-    if(!userId || !type || !chatId) {
-      return res.status(400).json({ message: 'Missing required information.' })
+    // Validation check for required information
+    if (!userId || !type || !chatId) {
+      return res.status(400).json({ message: 'Missing required information.' });
     }
 
-    const filter = type === 'room' ? [chatId] : [userId, chatId]
+    // Construct filter based on message type
+    const filter = type === 'room' ? [chatId] : [userId, chatId];
+    // Query messages from database
     const messages = await Message
       .find()
       .all('users', filter)
       .sort({ createdAt: 1 })
-      .lean()
+      .lean();
     
+    // Get sender's avatar for each message
     const messagesWithAvatar = await Promise.all(
       messages.map(async(msg) => {
-        const senderId = msg.sender
-        const user = await User.findById(senderId).lean()
+        const senderId = msg.sender;
+        const user = await User.findById(senderId).lean();
         return {
           ...msg,
           avatarImage: user.avatarImage
-        }
+        };
       })
-    )
+    );
 
-    return res.status(200).json({ data: messagesWithAvatar })
+    return res.status(200).json({ data: messagesWithAvatar });
   } catch(err) {
-    return res.status(404).json({ message: err.message })
+    return res.status(404).json({ message: err.message });
   }
-})
+});
 
-// CREATE
+// Route handler to post a user message
 const postUserMessage = asyncHandler(async(req, res) => {
   try {
-    const { userId } = req.params
-    const { chatId } = req.query
-    const { message } = req.body
+    const { userId } = req.params;
+    const { chatId } = req.query;
+    const { message } = req.body;
 
-    if(!userId || !chatId || !message) {
-      return res.status(400).json({ message: 'Missing required information.' })
+    // Validation check for required information
+    if (!userId || !chatId || !message) {
+      return res.status(400).json({ message: 'Missing required information.' });
     }
 
+    // Create new message
     const newMessage = await Message.create({
       message,
       users: [userId, chatId],
       sender: userId,
       readers: []
-    })
+    });
 
-    return res.status(200).json({ data: newMessage })
+    return res.status(200).json({ data: newMessage });
 
   } catch(err) {
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message });
   }
-})
+});
 
+// Route handler to post a room
 const postRoom = asyncHandler(async (req, res, next) => {
   try {
-    const { userId } = req.params
-    const { name, users,  avatarImage } = req.body
+    const { userId } = req.params;
+    const { name, users,  avatarImage } = req.body;
 
-    if(!userId || !name || !users || !avatarImage) {
-      return res.status(400).json({ message: 'Missing required information.' })
+    // Validation check for required information
+    if (!userId || !name || !users || !avatarImage) {
+      return res.status(400).json({ message: 'Missing required information.' });
     }
 
+    // Create new room
     const data = await Room.create({
       name,
       users: [...users, userId],
       avatarImage,
       chatType: 'room'
-    })
+    });
 
-    return res.json({ data, messages: 'Successfully created a room.' })
+    return res.json({ data, messages: 'Successfully created a room.' });
 
   } catch(err) {
-    return res.status(500).json({ message: e.message })
+    return res.status(500).json({ message: e.message });
   }
-})
+});
 
-// UPDATE
+// Route handler to update message read status
 const updateMessageReadStatus = asyncHandler(async (req, res) => {
   try {
-    // chatId 的訊息被 userId 已讀
-    const { userId } = req.params
-    const { type, chatId } = req.query
+    // Update message read status
+    const { userId } = req.params;
+    const { type, chatId } = req.query;
 
-    if(!userId || !type || !chatId) {
-      return res.status(400).json({ message: 'Missing required information.' })
+    // Validation check for required information
+    if (!userId || !type || !chatId) {
+      return res.status(400).json({ message: 'Missing required information.' });
     }
 
-    const filter = type === 'room' ? [chatId] : [userId, chatId]
+    const filter = type === 'room' ? [chatId] : [userId, chatId];
 
-    // 撈出所有 chat 中 sender 不是自己的 message
+    // Query messages where sender is not the user
     const messages = await Message
       .find({ sender: { $ne: userId }})
       .all('users', filter)
-      .sort({ createdAt: 1 })
+      .sort({ createdAt: 1 });
 
-    // 取得 message 和 reader 的對應值
+    // Create message-reader map
     const messageReaderMap = messages.reduce((prev, curr) => {
-      return {...prev, [curr._id.toHexString()]: curr.readers }
-    }, {})
+      return {...prev, [curr._id.toHexString()]: curr.readers };
+    }, {});
 
-    // 檢查 userId 是否已存在 readers 裡 -> 不存在則新增
+    // Check if user already exists in readers, if not, add them
     Object.entries(messageReaderMap).forEach(([key, value]) => {
-      const userHasRead = value.indexOf(userId) > -1
-      if (!userHasRead) messageReaderMap[key].push(userId) // 還沒已讀就加入
-    })
+      const userHasRead  = value.indexOf(userId) > -1;
+      if (!userHasRead) {
+        messageReaderMap[key].push(userId); // If not read yet, add the user
+      }
+    });
     
-    // 更新已讀
+    // Update read status for messages
     await Promise.all(
       Object.keys(messageReaderMap).map(async (msgId) => {
         return await Message
               .findByIdAndUpdate({ _id: msgId }, { readers: messageReaderMap[msgId] }, { new: true } )
-              .lean()
+              .lean();
       })
-    )
+    );
     
-    return res.status(200).json({ data: null, message: 'Successfully updated.'})
+    return res.status(200).json({ data: null, message: 'Successfully updated.'});
   } catch(err) {
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message });
   }
-})
-
-module.exports = {
-  getUserContacts,
-  getUserMessages,
-  postUserMessage,
-  postRoom,
-  updateMessageReadStatus
-}
+  });
+  
+  module.exports = {
+    getUserContacts,
+    getUserMessages,
+    postUserMessage,
+    postRoom,
+    updateMessageReadStatus
+  };
--- chat-app-react-nodejs-master/server/socket/index.js
+++ chat-app-react-nodejs-master/server/socket/index.js
@@ -1,120 +1,132 @@
-const Message = require('../model/Message')
+const Message = require('../model/Message'); // Importing the Message model
 
+// Function to initialize socket
 const initSocket = (server, corsOptions) => {
-  const io = require('socket.io')(server, { cors: corsOptions })
+  const io = require('socket.io')(server, { cors: corsOptions }); // Initializing socket.io with server and cors options
 
-  let onlineUsers = []
+  let onlineUsers = []; // Array to store online users
 
+  // Event listener for new connections
   io.on('connection', socket => {
+    // Event listener for when a user comes online
     socket.on('USER_ONLINE', (userId, socketId) => {
-      const userExisted = onlineUsers.some(user => user.userId === userId)
-      const prevSocketId = userExisted?.socketId || null
-      if (userExisted && prevSocketId !== socketId) {
-        onlineUsers = onlineUsers.map(user => {
-          return user.userId === userId ? ({ ...user, socketId: socketId }) : user
-        })
-      } else if (!userExisted) {
-        onlineUsers.push({
-          userId, 
-          socketId: socketId
-        })
-        io.emit('ONLINE_USER_CHANGED', onlineUsers)
+      const userExisted = onlineUsers.some(user => user.userId === userId); // Check if user exists
+      const prevSocketId = userExisted?.socketId || null; // Get previous socket id if user existed
+      if (userExisted && prevSocketId !== socketId) { // If user existed and has different socket id
+        onlineUsers = onlineUsers.map(user => user.userId === userId ? ({ ...user, socketId: socketId }) : user); // Update user's socket id
+      } else if (!userExisted) { // If user is not already online
+        onlineUsers.push({ userId, socketId: socketId }); // Add user to online users
+        io.emit('ONLINE_USER_CHANGED', onlineUsers); // Emit event to inform clients about online user change
       }
-    })
+    });
 
+    // Event listener for when a user goes offline
     socket.on('USER_OFFLINE', (logoutUserId) => {
-      onlineUsers = onlineUsers.filter(({ userId }) => userId !== logoutUserId)
-      io.emit('ONLINE_USER_CHANGED', onlineUsers)
-    })
+      onlineUsers = onlineUsers.filter(({ userId }) => userId !== logoutUserId); // Remove user from online users
+      io.emit('ONLINE_USER_CHANGED', onlineUsers); // Emit event to inform clients about online user change
+    });
 
+    // Event listener for sending messages
     socket.on('SEND_MESSAGE', async (messageData) => {
-      const { type, sender, receiver } = messageData
-        
-      const filter = type === 'room' ? [receiver] : [sender, receiver]
+      const { type, sender, receiver } = messageData; // Extract message data
+      // Construct filter based on message type
+      const filter = type === 'room' ? [receiver] : [sender, receiver];
+      // Query message readers from database
       const messageReader = await Message
         .find()
         .all('users', filter)
         .select(['readers'])
         .sort({ createdAt: -1 })
-        .lean()
-      
-      const unreadCount = messageReader.filter(({ readers }) => readers.length === 0).length
-      const clientId = type === 'user' ? onlineUsers.find(({ userId }) => userId === receiver)?.socketId : receiver
-      if (clientId) {
-        socket.to(clientId).emit('RECEIVE_MESSAGE', {...messageData, unreadCount});
+        .lean();
+      // Count unread messages
+      const unreadCount = messageReader.filter(({ readers }) => readers.length === 0).length;
+      // Get client id of receiver
+      const clientId = type === 'user' ? onlineUsers.find(({ userId }) => userId === receiver)?.socketId : receiver;
+      if (clientId) { // If client id is available
+        // Emit event to notify receiver about new message
+        socket.to(clientId).emit('RECEIVE_MESSAGE', { ...messageData, unreadCount });
       }
-    })
+    });
 
+    // Event listener for updating message status
     socket.on('UPDATE_MESSAGE_STATUS', ({ type, readerId, messageSender }) => {
-      // messageSender 的訊息被 readerId 已讀
+      // Find socket id of message sender
       const socketId = type === 'room' 
         ? messageSender 
-        : onlineUsers.find(({ userId }) => userId === messageSender)?.socketId
-      if (socketId) {
-        socket.to(socketId).emit('MESSAGE_READ', { type, readerId, messageSender })
+        : onlineUsers.find(({ userId }) => userId === messageSender)?.socketId;
+      if (socketId) { // If socket id is available
+        // Emit event to notify sender about message read status
+        socket.to(socketId).emit('MESSAGE_READ', { type, readerId, messageSender });
       }
-    })
+    });
 
+    // Event listener for updating message readers
     socket.on('UPDATE_MESSAGE_READERS', ({ type, readerId, toId }) => {
+      // Find socket id of message receiver
       const socketId = type === 'room' 
         ? toId 
-        : onlineUsers.find(({ userId }) => userId === toId)?.socketId
-      if (socketId) {
-        socket.to(socketId).emit('MESSAGE_READ', { type, readerId, toId })
+        : onlineUsers.find(({ userId }) => userId === toId)?.socketId;
+      if (socketId) { // If socket id is available
+        // Emit event to notify receiver about message read status
+        socket.to(socketId).emit('MESSAGE_READ', { type, readerId, toId });
       }
-    })
+    });
 
+    // Event listener for user typing status
     socket.on('USER_TYPING', ({ chatType, senderId, receiverId, typing, message }) => {
-      if (chatType === 'room') {
-        socket.to(receiverId).emit('TYPING_NOTIFY', { chatType, senderId, receiverId, typing, message })
-      } else {
-        const receiver = onlineUsers.find(({ userId }) => userId === receiverId)
-        if (receiver) {
-          socket.to(receiver.socketId).emit('TYPING_NOTIFY', { chatType, senderId, receiverId, typing, message })
+      if (chatType === 'room') { // If chat type is room
+        // Emit event to notify room members about typing status
+        socket.to(receiverId).emit('TYPING_NOTIFY', { chatType, senderId, receiverId, typing, message });
+      } else { // If chat type is user
+        const receiver = onlineUsers.find(({ userId }) => userId === receiverId); // Find receiver
+        if (receiver) { // If receiver is online
+          // Emit event to notify receiver about typing status
+          socket.to(receiver.socketId).emit('TYPING_NOTIFY', { chatType, senderId, receiverId, typing, message });
         }
       }
-    })
+    });
 
+    // Event listener for entering chat room
     socket.on('ENTER_CHAT_ROOM', roomData => {
-      const { roomId, message } = roomData
-      // 檢查是否已有房間
-      const currentRoom = Object.keys(socket.rooms).find(room => room !== socket.id)
-      if (currentRoom === roomId) return
-      // 若有，則先離開
-      if (currentRoom) {
-        socket.leave(currentRoom)
+      const { roomId, message } = roomData; // Extract room data
+      // Check if user is already in a room
+      const currentRoom = Object.keys(socket.rooms).find(room => room !== socket.id);
+      if (currentRoom === roomId) return; // If already in the requested room, do nothing
+      if (currentRoom) { // If in another room
+        socket.leave(currentRoom); // Leave current room
       }
-      // 加入新的
-      socket.join(roomId)
-      // 除了自己以外的人接收到訊息
+      socket.join(roomId); // Join requested room
+      // Emit event to notify room members about new user joining
       socket.to(roomId).emit('CHAT_ROOM_NOTIFY', {
         roomId,
         message
-      })
-    })
+      });
+    });
 
+    // Event listener for leaving chat room
     socket.on('LEAVE_CHAT_ROOM', roomData => {
-      const { roomId, message } = roomData
-      // 除了自己以外的人接收到訊息
+      const { roomId, message } = roomData; // Extract room data
+      // Emit event to notify room members about user leaving
       socket.to(roomId).emit('CHAT_ROOM_NOTIFY', {
         roomId,
         message
-      })
-      socket.leave(roomId)
-    })
+      });
+      socket.leave(roomId); // Leave room
+    });
     
+    // Event listener for room creation
     socket.on('ROOM_CREATED', ({ name, creator, invitedUser }) => {
       invitedUser.forEach(invitedUser => {
-        const socketId = onlineUsers.find(({ userId }) => userId === invitedUser)?.socketId
-        // 被邀請的人在線上才通知
-        if (socketId) {
-          socket.to(socketId).emit('INVITED_TO_ROOM', { message: `${creator} 已將你加入 ${name} 聊天室`})
+        const socketId = onlineUsers.find(({ userId }) => userId === invitedUser)?.socketId; // Find socket id of invited user
+        if (socketId) { // If invited user is online
+          // Emit event to notify invited user about room invitation
+          socket.to(socketId).emit('INVITED_TO_ROOM', { message: `${creator} has added you to ${name} chat room`});
         }
-      })
-    })
-  })
-}
+      });
+    });
+  });
+};
 
 module.exports = {
   initSocket
-}
+};
